6cf8264e1af19ed92936ae2500410f0f6774abdb
aryanneb <aryanneb007@gmail.com>
2020-08-24 16:41:55 -0700

    got the name changer working, moved the js to a new file

---
+++ 9181c.js
@@ -0,0 +1,22 @@
+let names = [
+  document.getElementById("name"),
+  document.getElementById("name2"),
+  document.getElementById("name3"),
+  document.getElementById("name4"),
+  document.getElementById("name5"),
+];
+
+textChanger(0);
+
+function textChanger(i) {
+  if (names.length > i) {
+    names[i].style.display = "block";
+    names[i].style.opacity = "1";
+    setTimeout(function () {
+      names[i].style.display = "none";
+      textChanger(++i);
+    }, 2000);
+  } else if (names.length == i) {
+    textChanger(0);
+  }
+}
