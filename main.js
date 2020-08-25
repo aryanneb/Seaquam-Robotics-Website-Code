dde18f2850b99c156bb2bac630138e7639189888
aryanneb <aryanneb007@gmail.com>
2020-08-25 16:31:35 -0700

    added the basic navigation for the home.html

--- main.js
+++ main.js
@@ -1,4 +1,30 @@
 window.addEventListener("scroll", function () {
-    var header = document.querySelector("header");
-    header.classList.toggle("sticky", window.scrollY > 175);
+  var header = document.querySelector("header");
+  header.classList.toggle("sticky", window.scrollY > 175);
+});
+
+// Navigation menu animation
+
+const navSlide = () => {
+  const burger = document.querySelector(".burger");
+  const nav = document.querySelector(".nav");
+  const navLinks = document.querySelectorAll(".main-link");
+
+  burger.addEventListener("click", () => {
+    nav.classList.toggle("nav-toggle");
+
+    navLinks.forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = "";
+      } else {
+        link.style.animation = `navLinkAni 0.4s ease forwards ${
+          index / 7 + 0.2
+        }s`;
+      }
+    });
+
+    burger.classList.toggle("toggle");
   });
+};
+
+navSlide();
