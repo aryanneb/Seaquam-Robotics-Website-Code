5aebc4ac2d3545424ec59f1d2316418e17e5b86e
aryanneb <aryanneb007@gmail.com>
2020-08-25 19:54:31 -0700

    got a basic dropdown working on the mobile view

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ window.addEventListener("scroll", function () {
 const navSlide = () => {
   const burger = document.querySelector(".burger");
   const nav = document.querySelector(".nav");
-  const navLinks = document.querySelectorAll(".main-link");
+  let navLinks = document.querySelectorAll(".main-link");
 
   burger.addEventListener("click", () => {
     nav.classList.toggle("nav-toggle");
@@ -28,3 +28,29 @@ const navSlide = () => {
 };
 
 navSlide();
+
+const historyDropdown = () => {
+  const parent = document.getElementsByClassName("nav")[0];
+  const reference = document.getElementsByClassName("main-links")[2];
+  const plus = document.getElementsByClassName("historyDrpDwn")[0];
+  const child1 = document.getElementsByClassName("history1")[0];
+  const child2 = document.getElementsByClassName("history2")[0];
+
+  plus.addEventListener("click", () => {
+    if (plus.classList.contains("dropped")) {
+      parent.removeChild(child1);
+      parent.removeChild(child2);
+      child1.style.opacity = "0";
+      child2.style.opacity = "0";
+      plus.classList.toggle("dropped");
+    } else {
+      parent.insertBefore(child1, parent.childNodes[4]);
+      parent.insertBefore(child2, parent.childNodes[5]);
+      child1.style.opacity = "1";
+      child2.style.opacity = "1";
+      plus.classList.toggle("dropped");
+    }
+  });
+};
+
+historyDropdown();
