95e2e2c2d3db21be242f5cac8918720b6834c3b1
aryanneb <aryanneb007@gmail.com>
2020-09-02 03:56:27 -0700

    added a quick scroll function on the learn more

---
+++ home.js
@@ -0,0 +1,11 @@
+const more = document.getElementsByClassName("home-button")[0];
+
+more.addEventListener('click', scrollDown);
+
+function scrollDown() {
+    window.scroll({
+        top: window.innerHeight / 100 * 90,
+        left: 0,
+        behavior: 'smooth'
+    });
+}
