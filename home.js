c035d24884fe6c5dc964b8ea90c0fbc52016cb9e
aryanneb <aryanneb007@gmail.com>
2020-10-27 00:01:18 -0700

    created nav.html, which is a common navbar.
    Added Jquery links to all the pages. Also changed a little bit of formatting in resources.css

--- home.js
+++ home.js
@@ -1,41 +1,41 @@
 const more = document.getElementsByClassName("home-button")[0];
 
-more.addEventListener('click', scrollDown);
+more.addEventListener("click", scrollDown);
 
 function scrollDown() {
-    window.scroll({
-        top: window.innerHeight / 100 * 90,
-        left: 0,
-        behavior: 'smooth'
-    });
+  window.scroll({
+    top: (window.innerHeight / 100) * 90,
+    left: 0,
+    behavior: "smooth",
+  });
 }
 
-
 // Navigation menu animation Desktop
+// Doesn't currently work :(
 anime({
-    targets: '.logo-box',
-    translateX: [-250, 0],
-    easing: 'spring(1, 80, 85, 2)',
-    duration: 800
-})
+  targets: ".logo-box",
+  translateX: [-250, 0],
+  easing: "spring(1, 80, 85, 2)",
+  duration: 800,
+});
 
 anime({
-    targets: '.main-link',
-    translateX: [250, 0],
-    easing: 'spring(1, 80, 85, 2)',
-    duration: 500,
-    delay: anime.stagger(100)
-})
+  targets: ".main-link",
+  translateX: [250, 0],
+  easing: "spring(1, 80, 85, 2)",
+  duration: 500,
+  delay: anime.stagger(100),
+});
 
 anime({
-    targets: '.burger',
-    translateX: [250, 0],
-    easing: 'spring(1, 80, 85, 2)',
-    duration: 800,
-})
+  targets: ".burger",
+  translateX: [250, 0],
+  easing: "spring(1, 80, 85, 2)",
+  duration: 800,
+});
 anime({
-    targets: '.home-content',
-    translateX: [-100, 0],
-    easing: 'spring(1, 80, 85, 2)',
-    duration: 10000,
-})
+  targets: ".home-content",
+  translateX: [-100, 0],
+  easing: "spring(1, 80, 85, 2)",
+  duration: 10000,
+});
