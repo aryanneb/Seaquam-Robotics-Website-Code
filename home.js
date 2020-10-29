342e68fa4a35713e3afcf11118f7d79d4be04c3c
JCakboy <36982207+JCakboy@users.noreply.github.com>
2020-10-28 17:12:04 -0700

    Fixed home page animation

--- home.js
+++ home.js
@@ -9,33 +9,3 @@ function scrollDown() {
     behavior: "smooth",
   });
 }
-
-// Navigation menu animation Desktop
-// Doesn't currently work :(
-anime({
-  targets: ".logo-box",
-  translateX: [-250, 0],
-  easing: "spring(1, 80, 85, 2)",
-  duration: 800,
-});
-
-anime({
-  targets: ".main-link",
-  translateX: [250, 0],
-  easing: "spring(1, 80, 85, 2)",
-  duration: 500,
-  delay: anime.stagger(100),
-});
-
-anime({
-  targets: ".burger",
-  translateX: [250, 0],
-  easing: "spring(1, 80, 85, 2)",
-  duration: 800,
-});
-anime({
-  targets: ".home-content",
-  translateX: [-100, 0],
-  easing: "spring(1, 80, 85, 2)",
-  duration: 10000,
-});
