ec4686b52468e6f494ada683e9e76523fb2f979b
JCakboy <36982207+JCakboy@users.noreply.github.com>
2021-02-11 23:51:30 -0800

    Javascript cleanup

--- resources/resourcepages.js
+++ resources/resourcepages.js
@@ -3,243 +3,49 @@ let activeList = document.getElementById("one");
 let activeUp = document.getElementById("welcomeBtn");
 let activeLo = document.getElementById("welcomeSub1");
 
-// lowerMenu buttons & lower inline buttons triggers
-document
-  .getElementById("welcomeSub1")
-  .addEventListener("click", welcome_1Visibility);
-
-document
-  .getElementById("welcomeSub2")
-  .addEventListener("click", welcome_2Visibility);
-
-document.getElementById("twoSub1").addEventListener("click", two_1Visibility);
-
-document.getElementById("twoSub2").addEventListener("click", two_2Visibility);
-
-document.getElementById("twoSub3").addEventListener("click", two_3Visibility);
-
-document
-  .getElementById("threeSub1")
-  .addEventListener("click", three_1Visibility);
-
-document
-  .getElementById("threeSub2")
-  .addEventListener("click", three_2Visibility);
-
-document
-  .getElementById("threeSub3")
-  .addEventListener("click", three_3Visibility);
-
-document
-  .getElementById("threeSub4")
-  .addEventListener("click", three_4Visibility);
-
-document.getElementById("fourSub1").addEventListener("click", four_1Visibility);
-
-document.getElementById("fourSub2").addEventListener("click", four_2Visibility);
-
-document.getElementById("fourSub3").addEventListener("click", four_3Visibility);
-
-document.getElementById("fourSub4").addEventListener("click", four_4Visibility);
-
-document.getElementById("fiveSub1").addEventListener("click", five_1Visibility);
-
-document.getElementById("fiveSub2").addEventListener("click", five_2Visibility);
-
-document.getElementById("fiveSub3").addEventListener("click", five_3Visibility);
-
 // lowermenu button & content visibility toggles
 
-function welcome_1Visibility() {
-  active.style.display = "none";
-  active = document.getElementsByClassName("welcome_1")[0];
-  active.style.display = "block";
-  activeLo.setAttribute("class", "button-style-menu lowerMenu");
-  activeLo = document.getElementById("welcomeSub1");
-  activeLo.setAttribute("class", "button-style-menu lowerMenuAct");
-  activeUp.setAttribute("class", "button-style-menu upperMenu");
-  activeUp = document.getElementById("welcomeBtn");
-  activeUp.setAttribute("class", "button-style-menu upperMenuAct");
-}
+resourceVisibilityKeys = {
+  welcomeSub1: ["welcome_1", "welcomeSub1", "welcomeBtn"],
+  welcomeSub2: ["welcome_2", "welcomeSub2", "welcomeBtn"],
+  twoSub1: ["two_1", "twoSub1", "tutorialsBtn"],
+  twoSub2: ["two_2", "twoSub2", "tutorialsBtn"],
+  twoSub3: ["two_3", "twoSub3", "tutorialsBtn"],
+  threeSub1: ["three_1", "threeSub1", "vexcodeBtn"],
+  threeSub2: ["three_2", "threeSub2", "vexcodeBtn"],
+  threeSub3: ["three_3", "threeSub3", "vexcodeBtn"],
+  threeSub4: ["three_4", "threeSub4", "vexcodeBtn"],
+  fourSub1: ["four_1", "fourSub1", "prosBtn"],
+  fourSub2: ["four_2", "fourSub2", "prosBtn"],
+  fourSub3: ["four_3", "fourSub3", "prosBtn"],
+  fourSub4: ["four_4", "fourSub4", "prosBtn"],
+  fiveSub1: ["five_1", "fiveSub1", "designingBtn"],
+  fiveSub2: ["five_2", "fiveSub2", "designingBtn"],
+  fiveSub3: ["five_3", "fiveSub3", "designingBtn"],
+};
 
-function welcome_2Visibility() {
-  active.style.display = "none";
-  active = document.getElementsByClassName("welcome_2")[0];
-  active.style.display = "block";
-  activeLo.setAttribute("class", "button-style-menu lowerMenu");
-  activeLo = document.getElementById("welcomeSub2");
-  activeLo.setAttribute("class", "button-style-menu lowerMenuAct");
-  activeUp.setAttribute("class", "button-style-menu upperMenu");
-  activeUp = document.getElementById("welcomeBtn");
-  activeUp.setAttribute("class", "button-style-menu upperMenuAct");
-}
+for (var x in resourceVisibilityKeys)
+  if (document.getElementById(x) != null)
+    document.getElementById(x).addEventListener("click", setVisibilities);
 
-function two_1Visibility() {
-  active.style.display = "none";
-  active = document.getElementsByClassName("two_1")[0];
-  active.style.display = "block";
-  activeLo.setAttribute("class", "button-style-menu lowerMenu");
-  activeLo = document.getElementById("twoSub1");
-  activeLo.setAttribute("class", "button-style-menu lowerMenuAct");
-  activeUp.setAttribute("class", "button-style-menu upperMenu");
-  activeUp = document.getElementById("tutorialsBtn");
-  activeUp.setAttribute("class", "button-style-menu upperMenuAct");
-}
-
-function two_2Visibility() {
-  active.style.display = "none";
-  active = document.getElementsByClassName("two_2")[0];
-  active.style.display = "block";
-  activeLo.setAttribute("class", "button-style-menu lowerMenu");
-  activeLo = document.getElementById("twoSub2");
-  activeLo.setAttribute("class", "button-style-menu lowerMenuAct");
-  activeUp.setAttribute("class", "button-style-menu upperMenu");
-  activeUp = document.getElementById("tutorialsBtn");
-  activeUp.setAttribute("class", "button-style-menu upperMenuAct");
-}
-
-function two_3Visibility() {
-  active.style.display = "none";
-  active = document.getElementsByClassName("two_3")[0];
-  active.style.display = "block";
-  activeLo.setAttribute("class", "button-style-menu lowerMenu");
-  activeLo = document.getElementById("twoSub3");
-  activeLo.setAttribute("class", "button-style-menu lowerMenuAct");
-  activeUp.setAttribute("class", "button-style-menu upperMenu");
-  activeUp = document.getElementById("tutorialsBtn");
-  activeUp.setAttribute("class", "button-style-menu upperMenuAct");
-}
-
-function three_1Visibility() {
-  active.style.display = "none";
-  active = document.getElementsByClassName("three_1")[0];
-  active.style.display = "block";
-  activeLo.setAttribute("class", "button-style-menu lowerMenu");
-  activeLo = document.getElementById("threeSub1");
-  activeLo.setAttribute("class", "button-style-menu lowerMenuAct");
-  activeUp.setAttribute("class", "button-style-menu upperMenu");
-  activeUp = document.getElementById("vexcodeBtn");
-  activeUp.setAttribute("class", "button-style-menu upperMenuAct");
-}
-
-function three_2Visibility() {
-  active.style.display = "none";
-  active = document.getElementsByClassName("three_2")[0];
-  active.style.display = "block";
-  activeLo.setAttribute("class", "button-style-menu lowerMenu");
-  activeLo = document.getElementById("threeSub2");
-  activeLo.setAttribute("class", "button-style-menu lowerMenuAct");
-  activeUp.setAttribute("class", "button-style-menu upperMenu");
-  activeUp = document.getElementById("vexcodeBtn");
-  activeUp.setAttribute("class", "button-style-menu upperMenuAct");
-}
-
-function three_3Visibility() {
-  active.style.display = "none";
-  active = document.getElementsByClassName("three_3")[0];
-  active.style.display = "block";
-  activeLo.setAttribute("class", "button-style-menu lowerMenu");
-  activeLo = document.getElementById("threeSub3");
-  activeLo.setAttribute("class", "button-style-menu lowerMenuAct");
-  activeUp.setAttribute("class", "button-style-menu upperMenu");
-  activeUp = document.getElementById("vexcodeBtn");
-  activeUp.setAttribute("class", "button-style-menu upperMenuAct");
-}
-
-function three_4Visibility() {
-  active.style.display = "none";
-  active = document.getElementsByClassName("three_4")[0];
-  active.style.display = "block";
-  activeLo.setAttribute("class", "button-style-menu lowerMenu");
-  activeLo = document.getElementById("threeSub4");
-  activeLo.setAttribute("class", "button-style-menu lowerMenuAct");
-  activeUp.setAttribute("class", "button-style-menu upperMenu");
-  activeUp = document.getElementById("vexcodeBtn");
-  activeUp.setAttribute("class", "button-style-menu upperMenuAct");
-}
-
-function four_1Visibility() {
-  active.style.display = "none";
-  active = document.getElementsByClassName("four_1")[0];
-  active.style.display = "block";
-  activeLo.setAttribute("class", "button-style-menu lowerMenu");
-  activeLo = document.getElementById("fourSub1");
-  activeLo.setAttribute("class", "button-style-menu lowerMenuAct");
-  activeUp.setAttribute("class", "button-style-menu upperMenu");
-  activeUp = document.getElementById("prosBtn");
-  activeUp.setAttribute("class", "button-style-menu upperMenuAct");
-}
-
-function four_2Visibility() {
-  active.style.display = "none";
-  active = document.getElementsByClassName("four_2")[0];
-  active.style.display = "block";
-  activeLo.setAttribute("class", "button-style-menu lowerMenu");
-  activeLo = document.getElementById("fourSub2");
-  activeLo.setAttribute("class", "button-style-menu lowerMenuAct");
-  activeUp.setAttribute("class", "button-style-menu upperMenu");
-  activeUp = document.getElementById("prosBtn");
-  activeUp.setAttribute("class", "button-style-menu upperMenuAct");
-}
-
-function four_3Visibility() {
-  active.style.display = "none";
-  active = document.getElementsByClassName("four_3")[0];
-  active.style.display = "block";
-  activeLo.setAttribute("class", "button-style-menu lowerMenu");
-  activeLo = document.getElementById("fourSub3");
-  activeLo.setAttribute("class", "button-style-menu lowerMenuAct");
-  activeUp.setAttribute("class", "button-style-menu upperMenu");
-  activeUp = document.getElementById("prosBtn");
-  activeUp.setAttribute("class", "button-style-menu upperMenuAct");
-}
-
-function four_4Visibility() {
-  active.style.display = "none";
-  active = document.getElementsByClassName("four_4")[0];
-  active.style.display = "block";
-  activeLo.setAttribute("class", "button-style-menu lowerMenu");
-  activeLo = document.getElementById("fourSub4");
-  activeLo.setAttribute("class", "button-style-menu lowerMenuAct");
-  activeUp.setAttribute("class", "button-style-menu upperMenu");
-  activeUp = document.getElementById("prosBtn");
-  activeUp.setAttribute("class", "button-style-menu upperMenuAct");
-}
-
-function five_1Visibility() {
-  active.style.display = "none";
-  active = document.getElementsByClassName("five_1")[0];
-  active.style.display = "block";
-  activeLo.setAttribute("class", "button-style-menu lowerMenu");
-  activeLo = document.getElementById("fiveSub1");
-  activeLo.setAttribute("class", "button-style-menu lowerMenuAct");
-  activeUp.setAttribute("class", "button-style-menu upperMenu");
-  activeUp = document.getElementById("designingBtn");
-  activeUp.setAttribute("class", "button-style-menu upperMenuAct");
-}
-
-function five_2Visibility() {
-  active.style.display = "none";
-  active = document.getElementsByClassName("five_2")[0];
-  active.style.display = "block";
-  activeLo.setAttribute("class", "button-style-menu lowerMenu");
-  activeLo = document.getElementById("fiveSub2");
-  activeLo.setAttribute("class", "button-style-menu lowerMenuAct");
-  activeUp.setAttribute("class", "button-style-menu upperMenu");
-  activeUp = document.getElementById("designingBtn");
-  activeUp.setAttribute("class", "button-style-menu upperMenuAct");
-}
-
-function five_3Visibility() {
-  active.style.display = "none";
-  active = document.getElementsByClassName("five_3")[0];
-  active.style.display = "block";
-  activeLo.setAttribute("class", "button-style-menu lowerMenu");
-  activeLo = document.getElementById("fiveSub3");
-  activeLo.setAttribute("class", "button-style-menu lowerMenuAct");
-  activeUp.setAttribute("class", "button-style-menu upperMenu");
-  activeUp = document.getElementById("designingBtn");
-  activeUp.setAttribute("class", "button-style-menu upperMenuAct");
+function setVisibilities(eventobj) {
+  if (eventobj.target.id in resourceVisibilityKeys) {
+    active.style.display = "none";
+    active = document.getElementsByClassName(
+      resourceVisibilityKeys[eventobj.target.id][0]
+    )[0];
+    active.style.display = "block";
+    activeLo.setAttribute("class", "button-style-menu lowerMenu");
+    activeLo = document.getElementById(
+      resourceVisibilityKeys[eventobj.target.id][1]
+    );
+    activeLo.setAttribute("class", "button-style-menu lowerMenuAct");
+    activeUp.setAttribute("class", "button-style-menu upperMenu");
+    activeUp = document.getElementById(
+      resourceVisibilityKeys[eventobj.target.id][2]
+    );
+    activeUp.setAttribute("class", "button-style-menu upperMenuAct");
+  }
 }
 
 // mobile menu toggler
@@ -267,75 +73,3 @@ function scrollToContentTop() {
   window.scrollTo({ top: window.innerHeight * 1.18, behavior: "smooth" });
   // window.scrollBy(0, window.innerHeight*1.2);
 }
-
-//triggers scrollToContentTop
-
-[].forEach.call(document.getElementsByClassName("jump-button"), function (x) {
-  x.addEventListener("click", scrollToContentTop);
-});
-
-// get elements by second class of "jump-button" and triggers corresponding visibility
-// avoids onclick="xxx_xVisibility" on the "jump-buttons"
-[].forEach.call(document.getElementsByClassName("w-1"), function (x) {
-  x.addEventListener("click", welcome_1Visibility);
-});
-
-[].forEach.call(document.getElementsByClassName("w-2"), function (x) {
-  x.addEventListener("click", welcome_2Visibility);
-});
-
-[].forEach.call(document.getElementsByClassName("two-1"), function (x) {
-  x.addEventListener("click", two_1Visibility);
-});
-
-[].forEach.call(document.getElementsByClassName("two-2"), function (x) {
-  x.addEventListener("click", two_2Visibility);
-});
-
-[].forEach.call(document.getElementsByClassName("two-3"), function (x) {
-  x.addEventListener("click", two_3Visibility);
-});
-
-[].forEach.call(document.getElementsByClassName("three-1"), function (x) {
-  x.addEventListener("click", three_1Visibility);
-});
-
-[].forEach.call(document.getElementsByClassName("three-2"), function (x) {
-  x.addEventListener("click", three_2Visibility);
-});
-
-[].forEach.call(document.getElementsByClassName("three-3"), function (x) {
-  x.addEventListener("click", three_3Visibility);
-});
-
-[].forEach.call(document.getElementsByClassName("three-4"), function (x) {
-  x.addEventListener("click", three_4Visibility);
-});
-
-[].forEach.call(document.getElementsByClassName("four-1"), function (x) {
-  x.addEventListener("click", four_1Visibility);
-});
-
-[].forEach.call(document.getElementsByClassName("four-2"), function (x) {
-  x.addEventListener("click", four_2Visibility);
-});
-
-[].forEach.call(document.getElementsByClassName("four-3"), function (x) {
-  x.addEventListener("click", four_3Visibility);
-});
-
-[].forEach.call(document.getElementsByClassName("four-4"), function (x) {
-  x.addEventListener("click", four_4Visibility);
-});
-
-[].forEach.call(document.getElementsByClassName("five-1"), function (x) {
-  x.addEventListener("click", five_1Visibility);
-});
-
-[].forEach.call(document.getElementsByClassName("five-2"), function (x) {
-  x.addEventListener("click", five_2Visibility);
-});
-
-[].forEach.call(document.getElementsByClassName("five-3"), function (x) {
-  x.addEventListener("click", five_3Visibility);
-});
--- teams/teampages.js
+++ teams/teampages.js
@@ -1,102 +1,22 @@
-let modal = document.getElementById("modal");
-let modal2 = document.getElementById("modal2");
-let modal3 = document.getElementById("modal3");
-let modal4 = document.getElementById("modal4");
-let modal5 = document.getElementById("modal5");
-
-let img = document.getElementById("imgclick");
-let img2 = document.getElementById("imgclick2");
-let img3 = document.getElementById("imgclick3");
-let img4 = document.getElementById("imgclick4");
-let img5 = document.getElementById("imgclick5");
-
-let txt = document.getElementById("txtclick");
-let txt2 = document.getElementById("txtclick2");
-let txt3 = document.getElementById("txtclick3");
-let txt4 = document.getElementById("txtclick4");
-let txt5 = document.getElementById("txtclick5");
-
-let close = document.getElementById("close");
-let close2 = document.getElementById("close2");
-let close3 = document.getElementById("close3");
-let close4 = document.getElementById("close4");
-let close5 = document.getElementById("close5");
-
-img.onclick = function () {
-  modal.style.display = "flex";
-};
-
-txt.onclick = function () {
-  modal.style.display = "flex";
-};
-
-img2.onclick = function () {
-  modal2.style.display = "flex";
-};
-
-txt2.onclick = function () {
-  modal2.style.display = "flex";
-};
-
-img3.onclick = function () {
-  modal3.style.display = "flex";
-};
-
-txt3.onclick = function () {
-  modal3.style.display = "flex";
-};
-
-img4.onclick = function () {
-  modal4.style.display = "flex";
-};
-
-txt4.onclick = function () {
-  modal4.style.display = "flex";
-};
-
-img5.onclick = function () {
-  modal5.style.display = "flex";
-};
-
-txt5.onclick = function () {
-  modal5.style.display = "flex";
-};
-
-close.onclick = function () {
-  modal.style.display = "none";
-};
-
-close2.onclick = function () {
-  modal2.style.display = "none";
-};
-
-close3.onclick = function () {
-  modal3.style.display = "none";
-};
-
-close4.onclick = function () {
-  modal4.style.display = "none";
-};
-
-close5.onclick = function () {
-  modal5.style.display = "none";
-};
-
-window.onclick = function (event) {
-  if (
-    event.target == modal ||
-    event.target == modal2 ||
-    event.target == modal3 ||
-    event.target == modal4 ||
-    event.target == modal5
-  ) {
-    modal.style.display = "none";
-    modal2.style.display = "none";
-    modal3.style.display = "none";
-    modal4.style.display = "none";
-    modal5.style.display = "none";
-  }
-};
+function setModals() {
+  let keys = { imgclick: "flex", txtclick: "flex", close: "none" };
+  let iterations = 5;
+  for (let s in keys)
+    for (let i = 1; i <= iterations; i++) {
+      let targetid = s + (i == 1 ? "" : String(i));
+      let modalid = "modal" + (i == 1 ? "" : String(i));
+      document.getElementById(targetid).onclick = () =>
+        (document.getElementById(modalid).style.display = keys[s]);
+    }
+  window.onclick = (event) => {
+    if (event.target.classList.contains("modal"))
+      for (let i = 1; i <= iterations; i++)
+        document.getElementById(
+          "modal" + (i == 1 ? "" : String(i))
+        ).style.display = "none";
+  };
+}
+setModals();
 
 // Formats awards for a team. Use in conjunction with getTeamEventsAwards()
 async function formatTeamAwards(number) {
